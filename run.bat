b0b67d5f459fd34aa6493da28ab90baa660ec0b5
LifeMushroom <mathmaster01@gmail.com>
2014-09-18 22:02:43 -0400

    Create run.bat

---
+++ run.bat
@@ -0,0 +1,15 @@
+@echo OFF
+REM run.bat
+REM
+REM This only works if:
+REM - you have Python 3.4
+REM - you installed Python 3.4 to the default location
+REM - you installed all the other prerequisites (see readme.txt)
+REM - this file is in the same folder as newer_credits_editor.py
+REM - you're on Windows
+REM
+REM Usage: Double-click this to run the Newer Credits Editor from source.
+REM
+@echo ON
+
+C:/Python34/python.exe newer_credits_editor.py
